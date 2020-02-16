24aa7664a84750e75b3a35aeb2c4cd47f330ce57
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-02-16 22:20:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,12 +5,7 @@ def reverse_each_word(string)
   new_array.join(" ")
 end
 
-# def reverse_each_word(string)
-#   words_array = sentence2.split(" ")
-#   new_array = []
-#   new_array = words_array.each {|word| word.reverse}
-#   new_array.join(" ")
-# end
+
 
 
 
