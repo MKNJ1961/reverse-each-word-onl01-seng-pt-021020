a165ae1a0347946c261f95f1b35d4df4aa0b5d9a
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-02-16 20:53:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,11 @@
 def reverse_each_word(string)
   sentence1 = string.split(" ")
   return array = []
-  sentence1 array.ech do |string|
+  sentence1 array.each do |string|
     retrun array.reverse_each_word
   end
+  return array.join(" ")
+end
   
  
 
