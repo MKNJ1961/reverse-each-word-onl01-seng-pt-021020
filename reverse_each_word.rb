813ec09017457c39da341c6bc2e54112ca128c1c
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-02-16 22:59:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 def reverse_each_word(string)
   words_array = string.split(" ")
-  words_array.collect {|word| word.reverse }.join
+  words_array.collect {|word| word.reverse }.join(" ")
 
 end
 
