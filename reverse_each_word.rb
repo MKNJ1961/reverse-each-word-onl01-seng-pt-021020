16c4e268e16f609ea83a0c9f7ebc3bbd5d7a07de
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-02-16 21:04:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   words_array = sentence1.split(" ")
   new_array = []
-  new_array = words_array.collect {|word| word.reverse }
+  new_array = words_array.each {|word| word.reverse }
   new_array.join(" ")
 end
 
